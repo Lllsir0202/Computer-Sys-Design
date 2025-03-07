529bf13d37cbf0578834890f25f3ce4ccffe4a8d
tracer-ics2017 <tracer@njuics.org>
2025-03-07 18:19:15 +0800

    >  compile 161220000 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  18:19:15 up 53 min,  1 user,  load average: 0.00, 0.00, 0.00 918d429bc2690b2e314b539dc4e0e34c2daf4f82

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -149,7 +149,7 @@ static int cmd_x(char *args){
   // 这里需要提取出来内存地址，由于是32位的，所以我们可以使用uint32_t存即可
   vaddr_t addr;
   addr = strtoul(arg, NULL, 16);
-  // printf("%u\n", start_addr);
+  printf("0x%x\n", addr);
   int i;
   for(i = 0 ; i < N ; i++){
     // 首先输出内存地址
