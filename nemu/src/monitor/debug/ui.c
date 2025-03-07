74c8853885d647dd268d727bb9650c87c67cb55f
tracer-ics2017 <tracer@njuics.org>
2025-03-07 18:57:16 +0800

    >  compile 161220000 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  18:57:16 up  1:31,  1 user,  load average: 0.00, 0.00, 0.00 8ad0b9983e96acfffe970d0e773f7ced567de0f9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -157,7 +157,7 @@ static int cmd_x(char *args){
   for(i = 0 ; i < N ; i++){
     // 首先输出内存地址
     printf("0x%x\t", addr);
-    printf("%x\n", vaddr_read(addr, 4));
+    printf("0x%x\n", vaddr_read(addr, 4));
     addr = addr + 4;
   }
   return 0;
