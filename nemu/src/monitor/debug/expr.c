d5407ef70585169c6bb79ccc67696a6f65909f7f
tracer-ics2017 <tracer@njuics.org>
2025-03-11 20:09:01 +0800

    >  compile 161220000 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:09:01 up  9:18,  1 user,  load average: 0.00, 0.00, 0.00 9222ee4a3b61eb336b379a31673e0996c08099c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -365,7 +365,7 @@ uint32_t expr(char *e, bool *success) {
   *success = true;
   int i;
   for(i = 0 ; i < nr_token; i++){
-    printf("str is %s\n", tokens[i].str);
+    printf("str is %s\n index is %d", tokens[i].str, i);
     // 接下来就是需要去计算求值了
   }
 
