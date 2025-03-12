de70c138e5356044bf192a4d83cc40ab66d9fcb3
tracer-ics2017 <tracer@njuics.org>
2025-03-12 22:54:35 +0800

    >  compile 161220000 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:54:35 up  7:49,  1 user,  load average: 0.00, 0.00, 0.00 8e52112e3d2f8ae8c0530773916917854ca4d73f

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -21,6 +21,63 @@ void init_wp_pool() {
 /* TODO: Implement the functionality of watchpoint */
 // 用来实现wp的添加
 WP* new_up(){
-  return wp_pool+1;
+  // 首先我们需要在free中找一个，然后返回其的WP*即可
+  if(free_ == NULL){
+    // 说明没有了
+    panic("There is no free watchpoint");
+    assert(0);
+  }
+  // 这里说明free_是有空闲的
+  WP* new_wp = free_;
+  free_ = free_->next;
+  if(head == NULL){
+    head = new_wp;
+  }else{
+    WP* p = head;
+    while(p->next != NULL){
+      p = p->next;
+    }
+    // 这时p的next是NULL
+    p->next = new_wp;
+    new_wp->next = NULL;
+  }
+  return new_wp;
 }
 
+void free_up(WP* wp){
+  assert(wp != NULL);
+  WP* p = head;
+  assert(p != NULL);
+  if(head->NO == wp->NO){
+    // 说明头部就是
+    if(free_ == NULL){
+      free_ = wp;
+    }else{
+      WP* f_p = free_;
+      while(f_p->next != NULL){
+        f_p = f_p->next;
+      }
+      f_p->next = wp;
+    }
+    // 接下来就是把它从head中去除
+    head = NULL;
+    return ;
+  }
+  while(p->next != NULL){
+    // 当p不是尾部时，我们去检查即可
+    if(p->next->NO == wp->NO){
+      if(free_ == NULL){
+        free_ = wp;
+      }else{
+        WP* f_p = free_;
+        while(f_p->next != NULL){
+          f_p = f_p->next;
+        }
+        f_p->next = wp;
+      }
+      p->next = wp->next;
+      wp->next = NULL;
+      return ;
+    }
+  }
+}
