37bccb150324db55697e4d668a7138032eca7bf7
tracer-ics2017 <tracer@njuics.org>
2025-03-12 15:32:22 +0800

    >  compile 161220000 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:32:22 up 27 min,  1 user,  load average: 0.00, 0.00, 0.00 1afcebb6b1d9a24d838b9da2fda5c10a722717f3

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -8,7 +8,8 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  // 我们需要保存下来表达式，但是由于我们的实现expr时，解析和计算是同步进行的，所以我们只能存下char*
+  char* expression;
 
 } WP;
 
