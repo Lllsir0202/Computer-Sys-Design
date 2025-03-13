6f48b375669f092545b30e7f866e874988c4a453
tracer-ics2017 <tracer@njuics.org>
2025-03-13 15:08:50 +0800

    >  compile 161220000 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:08:50 up  1:09,  1 user,  load average: 0.00, 0.00, 0.00 166e8a2c39c52dabe9eb19b32640f627a6ab52e3

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -191,7 +191,7 @@ static int cmd_x(char *args){
 static int cmd_w(char *args){
   char* arg = strtok(NULL, "");
   WP* wp = new_up(arg);
-  printf("%s",wp->expression);
+  // printf("%s",wp->expression);
   bool success;
   uint32_t result = expr(arg, &success);
   if(!success){
