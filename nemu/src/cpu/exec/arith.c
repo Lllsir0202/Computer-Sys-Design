6ced4c680b2d87d15bb90ad7f3628c3569e73f78
tracer-ics2017 <tracer@njuics.org>
2025-03-29 15:21:41 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:21:41 up  1:28,  1 user,  load average: 0.00, 0.00, 0.00 54f72f35365de8d126299bcb5e97cd8e457397

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,7 +7,7 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  Log("The dest op is %x", id_dest->val);
+  Log("The dest op is %x", id_dest->reg);
   Log("The src op is %x", id_src->val);
 
   print_asm_template2(sub);
