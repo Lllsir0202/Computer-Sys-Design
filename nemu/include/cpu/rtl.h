81f7a85a2fee3ee064f4ba26af061076c959df18
tracer-ics2017 <tracer@njuics.org>
2025-03-29 16:11:01 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:11:01 up  2:18,  1 user,  load average: 0.00, 0.00, 0.00 a1c7c850b81a7dd8159926b025d445b26e4a0636

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -143,7 +143,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   cpu.esp = cpu.esp - 4;
-  vaddr_write(cpu.esp, 4, *src1);
+  vaddr_write(cpu.esp, *src1, 4);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
