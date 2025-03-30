9acc2277dcec6626ed0921acf2743c99bd267592
tracer-ics2017 <tracer@njuics.org>
2025-03-30 18:42:13 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  18:42:13 up  4:45,  1 user,  load average: 0.00, 0.00, 0.00 f1539c5f26bd38c7e05d01cd2adc4efffb796e66

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -112,7 +112,7 @@ opcode_entry opcode_table [512] = {
   /* 0x5c */	IDEX(pop_r, pop), IDEX(pop_r, pop), IDEX(pop_r, pop), IDEX(pop_r, pop), // 0x58-0x5F pop r32
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EX(operand_size), EMPTY,
-  /* 0x68 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x68 */	IDEXW(push_SI, push, 1), EMPTY, EMPTY, EMPTY, // 0x6a push
   /* 0x6c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x70 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x74 */	IDEXW(J,jcc,1), EMPTY, EMPTY, EMPTY, // 0x74 jz
