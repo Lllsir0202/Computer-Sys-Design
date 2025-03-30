a93605dd68c8e03ec92500dbd6f4e4046bf06313
tracer-ics2017 <tracer@njuics.org>
2025-03-30 16:26:47 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:26:47 up  2:30,  1 user,  load average: 0.00, 0.00, 0.00 39d33bbbe3fb1be23ef41d5b12d74963af3fd3f6

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,13 +1,16 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov);
-// Add: used in exec
+// Add: in pa2-1 used in exec
 make_EHelper(call);
 make_EHelper(push);
 make_EHelper(pop);
 make_EHelper(sub);
 make_EHelper(xor);
 make_EHelper(ret);
+
+//Add: in pa2-2
+make_EHelper(lea);
 
 make_EHelper(operand_size);
 
