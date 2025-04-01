ffb44a942d5c6c0629faa996a4b6c2e164cf932d
tracer-ics2017 <tracer@njuics.org>
2025-04-01 19:19:26 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  19:19:26 up 0 min,  1 user,  load average: 0.49, 0.16, 0.06 fd25c519f64a4f6da4168a8fd6bafc2f9c1c4168

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -44,7 +44,7 @@ make_EHelper(call_rm) {
   decoding.is_jmp = 1;
   rtl_push(&decoding.seq_eip);
   // Log("id_src is %x", id_src->val);
-  Log("id_dest val is %x, width is %d, seq_eip is %x", id_dest->val, id_dest->width, decoding.seq_eip);
+  // Log("id_dest val is %x, width is %d, seq_eip is %x", id_dest->val, id_dest->width, decoding.seq_eip);
   decoding.jmp_eip = vaddr_read(id_dest->val, id_dest->width);
   // Log("jmp dest is %x", decoding.jmp_eip);
 
