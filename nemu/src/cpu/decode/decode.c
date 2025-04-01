bb22405fbb5720bd8291df529ba98084418f4cdd
tracer-ics2017 <tracer@njuics.org>
2025-04-01 16:42:19 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:42:19 up  1:52,  1 user,  load average: 0.00, 0.00, 0.00 e0b4a2d94097445ed743b838c7a0f37a329e67

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -271,10 +271,10 @@ make_DHelper(a2O) {
 make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
-  Log("id_dest->simm is %d", id_dest->simm);
-  Log("eip is %x", *eip);
-  decoding.jmp_eip = ((int32_t)(id_dest->simm)) + *eip;
-  Log("decoding.jmp_eip is %x", decoding.jmp_eip);
+  // Log("id_dest->simm is %d", id_dest->simm);
+  // Log("eip is %x", *eip);
+  decoding.jmp_eip = id_dest->simm + *eip;
+  // Log("decoding.jmp_eip is %x", decoding.jmp_eip);
 }
 
 make_DHelper(push_SI) {
