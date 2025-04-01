2c9f8c62bffa78425898d9219c51762fc7f69917
tracer-ics2017 <tracer@njuics.org>
2025-04-01 20:35:31 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  20:35:31 up 47 min,  1 user,  load average: 0.00, 0.00, 0.00 211accbf49941cf817e20ad0a415ca2f63177a83

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -231,7 +231,7 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
   
-  uint32_t mask = (1U << (width << 3)) - 1;
+  uint32_t mask = (1U << ((4 - width) << 3)) - 1;
   cpu.EFLAGS.ZF = (((*result) & mask) == 0) ? 1 : 0;
   
   Log("Width is %d", width);
