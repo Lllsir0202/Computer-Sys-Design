dd9f8437b8850af9568a1f3affb0dccff8201d93
tracer-ics2017 <tracer@njuics.org>
2025-04-01 17:05:31 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  17:05:31 up  2:15,  1 user,  load average: 0.00, 0.00, 0.00 1be6bbd6339e666e71cdde8aaff6006dd6aac

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -93,7 +93,7 @@ make_group(gp7,
 // 在这里似乎应该就是使用opcode去索引 译码函数, 执行函数, 以及操作数宽度
 // 目前不懂如何去索引两个0xfc的数据
 opcode_entry opcode_table [512] = {
-  /* 0x00 */	EMPTY, EMPTY, EMPTY, IDEX(E2G,add),
+  /* 0x00 */	EMPTY, IDEX(E2G,add), EMPTY, IDEX(E2G,add), // 0x01
   /* 0x04 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x08 */	EMPTY, IDEX(E2G,or), EMPTY, EMPTY,
   /* 0x0c */	EMPTY, EMPTY, EMPTY, EX(2byte_esc),
