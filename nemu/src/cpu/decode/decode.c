6212a38bbba2745d010a354684bae4721aa21043
tracer-ics2017 <tracer@njuics.org>
2025-04-01 16:40:46 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:40:46 up  1:51,  1 user,  load average: 0.00, 0.00, 0.00 3a744367aaf7cee524e838302f6ec009af8860ac

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -40,7 +40,7 @@ static inline make_DopHelper(SI) {
    */
   uint32_t uvalue = instr_fetch(eip, op->width);
   // bug！！！
-  // 如果直接转换，由于这里可能只有1个byte，直接转换不会处理，所以我们不能这么做
+  // 如果直接转换，由于这里可能只有1个byte，直接转换不会处理，所以我们不能这么做 -> bingo
   op->simm = (op->width == 1) ? ((int32_t)(int8_t)uvalue) : ((int32_t)uvalue);
 
   rtl_li(&op->val, op->simm);
