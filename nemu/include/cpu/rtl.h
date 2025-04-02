c3ef24c1d8ece0c0bc5da5e336d4f5981c6e1f07
tracer-ics2017 <tracer@njuics.org>
2025-04-02 09:16:01 +0800

    >  compile 2212878 loki Linux ics 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  09:16:01 up 33 min,  1 user,  load average: 0.08, 0.08, 0.19 34456e7d382e9a5e9def240ef79f420c9147e28f

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -245,9 +245,9 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   default:
     Assert(0, "width is not 1, 2 or 4");
   }
-  Log("Result is %x", *result);
-  Log("Mask is %x", mask);
-  Log("eflags.ZF is %d", (((*result) & mask) == 0) ? 1 : 0);
+  // Log("Result is %x", *result);
+  // Log("Mask is %x", mask);
+  // Log("eflags.ZF is %d", (((*result) & mask) == 0) ? 1 : 0);
   cpu.EFLAGS.ZF = (((*result) & mask) == 0) ? 1 : 0;
 }
 
