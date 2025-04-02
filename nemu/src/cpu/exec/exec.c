0121f19a689b07785b9bacbacff9013eb6a7be16
tracer-ics2017 <tracer@njuics.org>
2025-04-02 22:01:31 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:01:31 up  2:08,  2 users,  load average: 0.01, 0.03, 0.01 2419370828dc2a4a2b7298dc2c25944d7b0fd159

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -53,8 +53,8 @@ static make_EHelper(name) { \
 // 06	-> XOR
 // 07	-> CMP
 make_group(gp1,
-    EX(add), EMPTY, EMPTY, EX(sbb),
-    EX(and), EX(sub), EMPTY, EX(cmp))
+    EX(add), EX(or), EX(adc), EX(sbb),
+    EX(and), EX(sub), EX(xor), EX(cmp))
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
   // 07 -> sar
