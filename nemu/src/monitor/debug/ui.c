ebe203b74aa029f5f2b18543ea686b5bedde4478
tracer-ics2017 <tracer@njuics.org>
2025-04-02 15:09:49 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:09:49 up 44 min,  1 user,  load average: 0.00, 0.00, 0.00 9441f28a27c5bf4bb7dd2ee8e70da3e68202dd

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -134,10 +134,10 @@ static int cmd_info(char *args){
     printf("eip\t");
     printf("0x%08x\n",cpu.eip);
     printf("eflags\t");
-    printf("\tZF: 0x%x\n",cpu.EFLAGS.ZF);
-    printf("\tSF: 0x%x\n",cpu.EFLAGS.SF);
-    printf("\tOF: 0x%x\n",cpu.EFLAGS.OF);
-    printf("\tCF: 0x%x\n",cpu.EFLAGS.CF);
+    printf("\tZF: 0x%x\n\t",cpu.EFLAGS.ZF);
+    printf("\tSF: 0x%x\n\t",cpu.EFLAGS.SF);
+    printf("\tOF: 0x%x\n\t",cpu.EFLAGS.OF);
+    printf("\tCF: 0x%x\n\t",cpu.EFLAGS.CF);
   }else if(strcmp(arg, "w") == 0){
     show_all();
   }else{
