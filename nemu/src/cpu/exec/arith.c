8e53134ea9a43a102977feaee3e4b66063ecd185
tracer-ics2017 <tracer@njuics.org>
2025-04-02 23:22:44 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:22:44 up  3:30,  2 users,  load average: 0.68, 0.47, 0.40 92dfcbbcfced9557f183159fd17f11ee55ddee

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -53,7 +53,7 @@ make_EHelper(sub) {
   rtl_msb(&t2, &t2, id_dest->width);
   rtl_set_OF(&t2);
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
-  // Log("id_dest->val is %x, id_src->val is %x, t0 is %x", id_dest->val, id_src->val, t0);
+  Log("id_dest->val is %x, id_src->val is %x, t0 is %x", id_dest->val, id_src->val, t0);
 
   print_asm_template2(sub);
 }
