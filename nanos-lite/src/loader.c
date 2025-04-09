5710812fff810f034ec12a4cee4defc15260d20d
tracer-ics2017 <tracer@njuics.org>
2025-04-09 19:41:38 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:41:38 up 28 min,  1 user,  load average: 0.08, 0.02, 0.01 ee063f7cee88f7f4d1aa085e78c3204697a55305

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,18 @@
 #include "common.h"
+extern void ramdisk_read(void *buf, size_t offset, size_t len);
+extern void ramdisk_write(void *buf, size_t offset, size_t len);
+extern size_t get_ramdisk_size();
+extern int ramdisk_start;
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  size_t len = get_ramdisk_size();
+  char ramdisk_buf[len];
+  if (ramdisk_buf == NULL) {
+    panic("Failed to allocate memory for ramdisk buffer");
+  }
+  ramdisk_read(ramdisk_buf, ramdisk_start, len);
+  ramdisk_write(ramdisk_buf, 0x4000000, len);
   return (uintptr_t)DEFAULT_ENTRY;
 }
