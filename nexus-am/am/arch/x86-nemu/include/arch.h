fb95083041e131330a297b490c7ae970a6ad4802
tracer-ics2017 <tracer@njuics.org>
2025-04-10 22:43:05 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:43:05 up  3:46,  1 user,  load average: 0.00, 0.00, 0.00 49ee11fba176d3f8c962145f66487f64891af3bf

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -6,9 +6,19 @@
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PGSIZE    4096    // Bytes mapped by a page
 
+// struct _RegSet {
+//   uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
+//   int       irq;
+// };
+
 struct _RegSet {
-  uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
+  // 越早压栈的,应该越后弹出,这里模拟的就是trap frame,所以我们根据push反过来处理即可
+  // 最后是通用寄存器
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  // 然后依次push的error_code和irq;
   int       irq;
+  // 栈底是EFLAGS, CS, EIP
+  uintptr_t eip, cs, eflags, error_code;
 };
 
 #define SYSCALL_ARG1(r) 0
