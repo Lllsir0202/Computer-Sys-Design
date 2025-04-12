388c7a18ed5af3dc431dbabbb87f256a8b3e5536
tracer-ics2017 <tracer@njuics.org>
2025-04-12 15:38:27 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:38:27 up 50 min,  1 user,  load average: 0.00, 0.00, 0.00 e0f1bc0ae746c212dccd714280f8aa4054c50f2b

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,6 +10,19 @@ static void sys_exit(uintptr_t param) {
   _halt(param);
 }
 
+static int sys_write(int fd, const void *buf, size_t count) {
+  if(fd == 1 || fd == 2){
+    // 1: stdout, 2: stderr
+    int i = 0;
+    for(i = 0; i < count; i++){
+      _putc(((char *)buf)[i]);
+    }
+    return count;
+  } else {
+    return -1;
+  }
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -23,6 +36,12 @@ _RegSet* do_syscall(_RegSet *r) {
       // 基于navy-apps/libs/libos/src/nanos.c中传递的参数，我们知道，
       // 其实eax->类型，ebx--edx -> 参数，而_exit传递的status就是在ebx中的
       sys_exit(SYSCALL_ARG2(r));
+      return r;
+    }
+    case SYS_write:{
+      // 基于man 2 write可以知道，三个参数分别是fd, buf, count
+      int ret_value = sys_write(SYSCALL_ARG2(r), (void *)SYSCALL_ARG3(r), SYSCALL_ARG4(r));
+      SYSCALL_ARG1(r) = ret_value;
       return r;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
