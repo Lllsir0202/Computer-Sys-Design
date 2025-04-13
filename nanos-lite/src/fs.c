4ae2e481ff03b3da257bc400cdc0e4c1b9dea9b9
tracer-ics2017 <tracer@njuics.org>
2025-04-13 20:34:52 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:34:52 up 20 min,  2 users,  load average: 0.02, 0.10, 0.09 ae69cd917e31e45dd329d38c9b1cf712d98a87f4

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -95,6 +95,7 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
       return 0;
     }
     case FD_EVENTS: {
+      Log("???");
       return events_read(buf, len);
     }
     case FD_DISPINFO: {
