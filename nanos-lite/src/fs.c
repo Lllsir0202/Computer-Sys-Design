78a1e963170d090cedf965fd401fddc28719fceb
tracer-ics2017 <tracer@njuics.org>
2025-04-13 16:30:57 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:30:57 up  3:10,  1 user,  load average: 0.00, 0.00, 0.00 9fdb4085f832778db810b0c1e9cfb60146a5279d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -36,7 +36,9 @@ void init_fs() {
   // 将后面的所有文件移动，避免交叉
   for(int i = FD_EVENTS; i < NR_FILES; i++) {
     file_table[i].disk_offset += size;
+    #ifdef DEBUG
     Log("file_table[%d].disk_offset is %d", i, file_table[i].disk_offset);
+    #endif
   }
   Log("fs initialize succeeds");
 }
