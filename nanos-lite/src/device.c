aafa10f21b7436b7765038aff64f0709aa22e92a
tracer-ics2017 <tracer@njuics.org>
2025-04-13 20:27:19 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:27:19 up 12 min,  2 users,  load average: 0.04, 0.18, 0.12 214638de41ea9e40fd936e666e5b866e2f118024

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -16,12 +16,14 @@ size_t events_read(void *buf, size_t len) {
     down = true;
   }
   if (key != _KEY_NONE) {
+    printf("%s %s\n", down ? "kd" : "ku" , keyname[key]);
     snprintf(buf, len, "%s %s\n", down ? "kd" : "ku" , keyname[key]);
-    Log("buf is %s", buf);
+    // Log("buf is %s", buf);
   }else{
     unsigned long ms = _uptime();
+    printf("t %u\n", ms);
     snprintf(buf, len, "t %u\n", ms);
-    Log("buf is %s", buf);
+    // Log("buf is %s", buf);
   }
   return strlen(buf);
 }
