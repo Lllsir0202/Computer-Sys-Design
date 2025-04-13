64d55d2f880b8a2ee66b987bd59354362daa6023
tracer-ics2017 <tracer@njuics.org>
2025-04-13 21:23:42 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:23:42 up  1:08,  2 users,  load average: 0.20, 0.10, 0.06 21d68d8b95c259472065a5ac6d263229954420db

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -6,7 +6,7 @@ extern size_t get_screen_size();
 extern int dispinfo_read(void *buf, off_t offset, size_t len);
 extern void fb_write(const void *buf, off_t offset, size_t len);
 extern int events_read(void *buf, size_t len);
-#define DEBUG
+// #define DEBUG
 
 typedef struct {
   char *name;         // 文件名
