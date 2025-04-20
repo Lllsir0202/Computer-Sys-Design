4edd04bbea83be3f3a673f480affebe71f336eb2
tracer-ics2017 <tracer@njuics.org>
2025-04-20 18:31:09 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:31:09 up  3:58,  1 user,  load average: 0.00, 0.00, 0.00 e6a5c8a0d290023d2c98606c4d62c90310bafee4

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -34,7 +34,7 @@ static inline paddr_t page_translate(vaddr_t addr, bool write) {
     // 这里的处理方式是直接panic
     panic("Page entry descriptor not present");
   }else if(!PDE_descriptor.present && write){
-    panic("error in write(PDE)");
+    // panic("error in write(PDE)");
   }
   PTE PTE_descriptor;
   data = paddr_read(PDE_descriptor.page_frame * PAGE_SIZE + PTE_index * sizeof(PTE), sizeof(PDE));
@@ -44,7 +44,7 @@ static inline paddr_t page_translate(vaddr_t addr, bool write) {
     // 这里的处理方式是直接panic
     panic("Page table descriptor not present");
   } else if(!PTE_descriptor.present && write){
-    panic("error in write(PTE)");
+    // panic("error in write(PTE)");
   }
   // 接下来即可拼接地址
   return (PTE_descriptor.page_frame << 12) | offset;
