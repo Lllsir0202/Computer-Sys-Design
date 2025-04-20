6f12df50964d0059cb0a121cb6eb7dba472ad0a0
tracer-ics2017 <tracer@njuics.org>
2025-04-20 16:35:36 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:35:36 up  2:03,  1 user,  load average: 0.06, 0.03, 0.01 7e28b8ce4c0d1d83342804d1e7985166126d9262

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -19,7 +19,9 @@ make_EHelper(lidt) {
 
 make_EHelper(mov_r2cr) {
   // 通过print_asm可以看到，其实cr0-cr3使用id_dest->reg来表示的
-  
+  Log("id_dest is %d", id_dest->reg);
+  Log("id_src->val is %d, id_src->val is %d", id_src->reg, id_src->val);
+  TODO();
 
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
