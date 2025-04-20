a73cf015998b863001c364556b614b2d95cd2d7b
tracer-ics2017 <tracer@njuics.org>
2025-04-20 17:43:57 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:43:57 up  3:11,  1 user,  load average: 0.00, 0.00, 0.00 5d1b3209a5c892d9851a1f9916d676b1937c23b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -14,8 +14,10 @@
 // pmem是数组，表示128MB的大内存RAM
 uint8_t pmem[PMEM_SIZE];
 
+static int cnt = 0;
 // ADD in pa4
 static inline paddr_t page_translate(vaddr_t addr, bool write) {
+  cnt++;
   // 现在不能够直接使用addr作为物理地址，因为需要进行页表的转换
   uint32_t PTD_index = (addr >> 22) & 0x3FF;
   uint32_t PTE_index = (addr >> 12) & 0x3FF;
@@ -32,6 +34,7 @@ static inline paddr_t page_translate(vaddr_t addr, bool write) {
   if(!PDE_descriptor.present){
     // 页表目录项没有present，说明没有映射
     // 这里的处理方式是直接panic
+    Log("cnt is %d", cnt);
     panic("Page entry descriptor not present");
   }
   PTE PTE_descripor;
@@ -40,6 +43,7 @@ static inline paddr_t page_translate(vaddr_t addr, bool write) {
   if(!PTE_descripor.present){
     // 页表项没有present，说明没有映射
     // 这里的处理方式是直接panic
+    Log("cnt is %d", cnt);
     panic("Page table descriptor not present");
   }
   // 接下来即可拼接地址
