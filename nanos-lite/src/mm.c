5956b25f73ea7da375b636ee03abdac6cd0a2609
tracer-ics2017 <tracer@njuics.org>
2025-04-28 22:13:32 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:13:32 up  2:01,  1 user,  load average: 0.00, 0.00, 0.00 5c56cd5cc535e223e7d94229c09c5bf81581e48c

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -17,6 +17,7 @@ void free_page(void *p) {
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
   if (current->cur_brk == 0) {
+    Log("first brk: %p", new_brk);
     current->cur_brk = current->max_brk = new_brk;
   }
   else {
@@ -25,23 +26,7 @@ int mm_brk(uint32_t new_brk) {
       // into address space current->as
 
       // 把current->as.area.start的虚拟空间映射到物理空间
-      // 计算需要映射的物理页面范围
-      uint32_t p_start = PGROUNDUP(current->max_brk);
-      uint32_t p_end = PGROUNDUP(new_brk);
       
-      // 获取虚拟地址起始点
-      // 这里假设堆区域从current->as.area.start开始
-      // 偏移量等于physical_brk相对于起始物理地址的距离
-      void *v_start = (void *)((uintptr_t)current->as.area.start + 
-                              (p_start - (uintptr_t)_heap.start));
-      
-      // 为每个物理页建立映射
-      for (uint32_t p_addr = p_start, v_addr = (uintptr_t)v_start; 
-           p_addr < p_end; 
-           p_addr += PGSIZE, v_addr += PGSIZE) {
-        // 将虚拟地址v_addr映射到物理地址p_addr
-        _map(&current->as, (void *)v_addr, (void *)p_addr);
-      }
 
       current->max_brk = new_brk;
     }
