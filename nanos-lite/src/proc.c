91845c7d46b8466f334d7a9fb108729e3a92e222
tracer-ics2017 <tracer@njuics.org>
2025-04-28 22:51:10 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:51:10 up  2:39,  1 user,  load average: 0.00, 0.02, 0.00 91745b124781cd656e01b238d971d220bf9d1f2c

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -18,6 +18,8 @@ void load_prog(const char *filename) {
 
   // TODO: remove the following three lines after you have implemented _umake()
   _switch(&pcb[i].as);
+  Log("switch to %s", filename);
+  
   current = &pcb[i];
   ((void (*)(void))entry)();
 
