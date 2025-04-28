331569c2a35639691b20d3e0dd99ba74dd0ec35b
tracer-ics2017 <tracer@njuics.org>
2025-04-28 20:31:06 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:31:06 up 19 min,  1 user,  load average: 0.02, 0.12, 0.08 1498d8f456931dddb5dfb8739e312ee8c48aa406

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -116,16 +116,19 @@ void vaddr_write(vaddr_t addr, int len, uint32_t data) {
       // 出现跨页，但是在指导书中的说法是只有跨页，但是不一定(?)，可能会有更多页？-> 不会有很多页的
       // 第一页先找到
       int first_page = PAGE_SIZE - offset;
-      
+      uint32_t data1 = data & (~0u >> (first_page << 3));
+      Log("data1 is %x", data1);
       // Log("first_page is %d", first_page);
       paddr_t paddr = page_translate(addr, false);
       // 写入第一页
-      paddr_write(paddr, first_page, (data & (~0u >> (first_page << 3))));
+      paddr_write(paddr, first_page, data1);
       // 读取第二页
       int second_page = len - first_page;
+      uint32_t data2 = data >> (first_page << 3);
+      Log("data2 is %x", data2);
       // Log("second_page is %d", second_page);
       paddr = page_translate(addr + first_page, false);
-      paddr_write(paddr, second_page, data >> (first_page << 3));
+      paddr_write(paddr, second_page, data2);
     }else {
       paddr_t paddr = page_translate(addr, true);
       // 这里的addr是虚拟地址，paddr是物理地址
