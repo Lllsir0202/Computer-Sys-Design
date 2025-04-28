9333c7685cfd7c382b92d1170b4966acb5d580f1
tracer-ics2017 <tracer@njuics.org>
2025-04-28 22:30:11 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:30:11 up  2:18,  1 user,  load average: 0.00, 0.00, 0.00 2c3c005531b27c19519c43f0eb37bac3ab965187

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -31,6 +31,7 @@ int mm_brk(uint32_t new_brk) {
       for(uint32_t addr = start; addr < end; addr += PGSIZE) {
         Log("mapping %p", addr);
         void *p = new_page();
+        Log("new page %p", p);
         if (p == NULL) {
           panic("no free page");
           return -1;
