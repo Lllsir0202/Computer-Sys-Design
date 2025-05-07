e0ae80e72b5fe40ca9b3c26678d08ff3885a0dc7
tracer-ics2017 <tracer@njuics.org>
2025-05-07 19:01:19 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:01:19 up  3:34,  1 user,  load average: 0.00, 0.00, 0.00 2716369206c42e9a69f182ff1aea88c509c1d41

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -38,7 +38,7 @@ uintptr_t loader(_Protect *as, const char *filename) {
       panic("Failed to allocate memory for page");
     }
     if((uintptr_t)page == 0x1d93000) {
-      Log("addr is %d", i);
+      Log("in loader !!!!!!addr is %d", i);
     }
     // Log("page is %p", page);
     fs_read(fd, page, PGSIZE);
