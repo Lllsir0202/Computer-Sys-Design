b4958a7125e5f300505dfc501b408d092d7186b2
tracer-ics2017 <tracer@njuics.org>
2025-05-07 19:59:20 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:59:20 up  4:32,  1 user,  load average: 0.00, 0.00, 0.00 cd46843590f27960f0c7a01fca977c112324eb94

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -91,6 +91,7 @@ void _map(_Protect *p, void *va, void *pa) {
   }
   else {
     // upte = (PTE*)PTE_ADDR(data);
+    _halt(-1);
   }
   // 这里的upte是一个物理页表的基址，然后我们可以通过PTE_index来找到对应的PTE
   PTE *pte = upte + PTE_index;
