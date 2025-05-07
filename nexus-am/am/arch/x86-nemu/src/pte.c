d72e3dfae1d380dbc90d93a733975a63c912ecce
tracer-ics2017 <tracer@njuics.org>
2025-05-07 20:10:00 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  20:10:00 up  4:43,  1 user,  load average: 0.00, 0.00, 0.00 c456c83a69beeda8cc9e3f809b7211b13c3707d3

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -95,7 +95,8 @@ void _map(_Protect *p, void *va, void *pa) {
   // 这里的upte是一个物理页表的基址，然后我们可以通过PTE_index来找到对应的PTE
   PTE *pte = upte + PTE_index;
   if(*pte & PTE_P) {
-    _halt(-1);
+    // 说明有多个va映射到同一个pa了
+    // _halt(-1);
   }
   *pte = (paddr & ~0xFFF) | PTE_P;
 }
