f628c02ae223223eac792f55c5bdc60be57f7596
tracer-ics2017 <tracer@njuics.org>
2025-05-07 18:40:20 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:40:20 up  3:13,  1 user,  load average: 0.00, 0.00, 0.00 78a04b18cde013365cc312e9c860ce5f9b7ae454

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -95,8 +95,8 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
       int second_page = len - first_page;
       // Log("second_page is %d", second_page);
       paddr = page_translate(addr + first_page, false);
-      if(addr == 0x8048a1c) {
-        Log("paddr is %x", paddr);
+      if(paddr == 0x1d93a1c) {
+        Log("in cross page!addr is %x", addr);
       }
       uint32_t data2 = paddr_read(paddr, second_page);
       // 这里的data是第一页的数据，data2是第二页的数据
@@ -104,8 +104,8 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
       return data2 << (8 * first_page) | data;
     }else {
       paddr_t paddr = page_translate(addr, false);
-      if(addr == 0x8048a1c) {
-        Log("paddr is %x", paddr);
+      if(paddr == 0x1d93a1c) {
+        Log("addr is %x", addr);
       }
       // 这里的addr是虚拟地址，paddr是物理地址
       return paddr_read(paddr, len);
