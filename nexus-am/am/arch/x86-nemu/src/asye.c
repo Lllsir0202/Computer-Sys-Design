95a0933eeaea04eb65b02d808cdd34367b886aa2
tracer-ics2017 <tracer@njuics.org>
2025-05-09 18:35:16 +0800

    >  compile 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:35:16 up 37 min,  1 user,  load average: 0.30, 0.09, 0.03 59121e2115f2dd348d07b7a213752d878db33ab

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -5,6 +5,7 @@ static _RegSet* (*H)(_Event, _RegSet*) = NULL;
 
 void vecsys();
 void vecnull();
+void vectrap();
 
 _RegSet* irq_handle(_RegSet *tf) {
   _RegSet *next = tf;
@@ -12,6 +13,7 @@ _RegSet* irq_handle(_RegSet *tf) {
     _Event ev;
     switch (tf->irq) {
       case 0x80: ev.event = _EVENT_SYSCALL; break;
+      case 0x81: ev.event = _EVENT_TRAP; break;
       default: ev.event = _EVENT_ERROR; break;
     }
 
@@ -34,6 +36,8 @@ void _asye_init(_RegSet*(*h)(_Event, _RegSet*)) {
 
   // -------------------- system call --------------------------
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_USER);
+  // -------------------- event trap --------------------------
+  idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_USER);
 
   // idt表示的是table，记录下总字节长度，再设置IDTR
   // 其实就是初始化设置idt
@@ -48,6 +52,7 @@ _RegSet *_make(_Area stack, void *entry, void *arg) {
 }
 
 void _trap() {
+  // 参考nano.c中的内联汇编写法
   asm volatile("int $0x81");
 }
 
