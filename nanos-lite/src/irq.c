e18b78ce4064b15c399e50bdc4b8f7c605944246
tracer-ics2017 <tracer@njuics.org>
2025-05-09 18:36:25 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:36:25 up 38 min,  1 user,  load average: 0.10, 0.07, 0.02 6ff84eb7e6bf993c69f1b23435340cc67f698935

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,6 +7,10 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
     case _EVENT_SYSCALL:{
       return do_syscall(r);
     }
+    case _EVENT_TRAP: {
+      Log("reach here");
+      return NULL;
+    }
     default: panic("Unhandled event ID = %d", e.event);
   }
 
