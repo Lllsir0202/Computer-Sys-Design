2fdd838002501d26dd9306e8aad12e21e7482103
tracer-ics2017 <tracer@njuics.org>
2025-05-13 19:53:44 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:53:44 up  1:27,  2 users,  load average: 0.59, 0.40, 0.22 ca3133f4fe1aca1c3f3df595aaed9a0f393d4747

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -9,6 +9,9 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
   printf("F_div_F: a = %d, b = %d\n", a, b);
+  if(b == 0) {
+    assert(0);
+  }
   return (a / b) << 16;
 }
 
