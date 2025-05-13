6b3948ebf7ffe5175b1508da5a2f403ae2223be8
tracer-ics2017 <tracer@njuics.org>
2025-05-13 21:01:30 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:01:30 up  2:35,  2 users,  load average: 0.29, 0.08, 0.02 d1ce396f756d539ee490111e29314953b0fe7da3

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -3,10 +3,31 @@
 #include <assert.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  printf("F_mul_F: a = %d, b = %d\n", a, b);
-  int64_t res = a * b;
-  printf("F_mul_F: a * b = %d\n", (int)(res >> 16));
-  return (int)(res >> 16);
+  // printf("F_mul_F: a = %d, b = %d\n", a, b);
+  // printf("F_mul_F: a * b = %d\n", (int)(a*b >> 16));
+  // 会出现溢出的Undefined Behavior
+  // 这里的a和b都是32位的整数,所以乘法会溢出
+  int sign = 1;
+  if((a < 0 && b > 0) || (a > 0 && b < 0)) {
+    sign = -1;
+  }
+  // 取绝对值
+  uint32_t ua = a < 0 ? -a : a;
+  uint32_t ub = b < 0 ? -b : b;
+  
+  // 拆分运算
+  uint32_t a_high = ua >> 16;
+  uint32_t a_low = ua & 0xFFFF;
+  uint32_t b_high = ub >> 16;
+  uint32_t b_low = ub & 0xFFFF;
+
+  uint32_t result = a_high * b_high;
+  uint32_t mid1 = a_high * b_low;
+  uint32_t mid2 = a_low * b_high;
+  uint32_t mid = (mid1 & 0xFFFF0000) + (mid2 & 0xFFFF0000) + (((mid1 & 0xFFFF) + (mid2 & 0xFFFF)) & 0xFFFF0000);
+  result += mid >> 16;
+  result += (a_low * b_low) >> 16;
+  return sign > 0 ? result : -result;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
