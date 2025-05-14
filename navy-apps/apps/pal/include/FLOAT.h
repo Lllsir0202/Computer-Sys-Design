2f46c191bfb26abd9adeb89069eb9e160be7396c
tracer-ics2017 <tracer@njuics.org>
2025-05-14 09:33:11 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:33:11 up  1:38,  2 users,  load average: 0.95, 1.02, 0.93 8b805847a867d42fb26601ed5be8ead975ce7870

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -4,36 +4,36 @@
 #include "assert.h"
 
 typedef int FLOAT;
-
-static inline int F2int(FLOAT a) {
-  // printf("F2int: %d\n", (int)a);
-  int sign = a & (1u << 31);
-  return sign ? -(-a >> 16) : (a >> 16);
-}
-
-static inline FLOAT int2F(int a) {
-  // printf("int2F: %d\n", a);
-  return a < 0 ? -(-a << 16) : (a << 16);
-}
-
-static inline FLOAT F_mul_int(FLOAT a, int b) {
-  // printf("F_mul_int: %d * %d\n", a, b);
-  return F_mul_F(a, int2F(b));
-}
-
-static inline FLOAT F_div_int(FLOAT a, int b) {
-  // printf("F_div_int: %d / %d\n", a, b);
-  if(b == 0) {
-    assert(0);
-  }
-  return F_div_F(a, int2F(b));
-}
-
 FLOAT f2F(float);
 FLOAT F_mul_F(FLOAT, FLOAT);
 FLOAT F_div_F(FLOAT, FLOAT);
 FLOAT Fabs(FLOAT);
 FLOAT Fsqrt(FLOAT);
 FLOAT Fpow(FLOAT, FLOAT);
+static inline int F2int(FLOAT a) {
+  printf("F2int: %d\n", (int)a);
+  int sign = a & (1u << 31);
+  return sign ? -((-a) >> 16) : (a >> 16);
+}
+
+static inline FLOAT int2F(int a) {
+  printf("int2F: %d\n", a);
+  return a < 0 ? -((-a) << 16) : (a << 16);
+}
+
+static inline FLOAT F_mul_int(FLOAT a, int b) {
+  printf("F_mul_int: %d * %d\n", a, b);
+  return F_mul_F(a, int2F(b));
+}
+
+static inline FLOAT F_div_int(FLOAT a, int b) {
+  printf("F_div_int: %d / %d\n", a, b);
+  if(b == 0) {
+    assert(0);
+  }
+  return F_div_F(a, int2F(b));
+}
+
+
 
 #endif
