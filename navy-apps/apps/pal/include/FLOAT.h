5c5f900c9e389d4e010e14ea92aded420b98e8df
tracer-ics2017 <tracer@njuics.org>
2025-05-23 10:20:21 +0800

    >  run 2212878 loki Linux ics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:20:21 up 42 min,  1 user,  load average: 0.51, 0.42, 0.37 46877069e1efffbb411e35fb274a7aec9738e1d3

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -7,22 +7,34 @@ typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
   // printf("F2int: %d\n", (int)a);
+  if((a>>16) == 0) {
+    assert(0);
+  }
   return (a >> 16);
 }
 
 static inline FLOAT int2F(int a) {
   // printf("int2F: %d\n", a);
+  if((a << 16) == 0) {
+    assert(0);
+  }
   return (a << 16);
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
   // printf("F_mul_int: %d * %d\n", a, b);
+  if(a * b == 0) {
+    assert(0);
+  }
   return a * b;
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
   // printf("F_div_int: %d / %d\n", a, b);
   if(b == 0) {
+    assert(0);
+  }
+  if(a / b == 0) {
     assert(0);
   }
   return a / b;
